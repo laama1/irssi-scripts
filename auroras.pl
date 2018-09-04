dbbd8be0ffc3e05f29d5a6bc94eca2f5c3e86e4f
mini <mini@mini.kissala>
2018-09-04 21:55:07 +0300

    cleaning

--- auroras.pl
+++ auroras.pl
@@ -4,14 +4,11 @@ use warnings;
 use Irssi;
 use vars qw($VERSION %IRSSI);
 use Irssi::Irc;
-#use HTML::Entities;
-#use LWP::UserAgent;
-#use LWP::Simple;
 use Data::Dumper;
 
 require KaaosRadioClass;
 
-$VERSION = '0.45';
+$VERSION = '0.46';
 %IRSSI = (
 'authors' => 'LAama1',
 'contact' => 'LAama1@ircnet',
@@ -19,18 +16,16 @@ $VERSION = '0.45';
 'description' => 'Kaaosradion Revontuli- ja kuun vaiheet -skripti.',
 'license' => 'BSD',
 'url' => 'http://www.kaaosradio.fi',
-'changed' => '2017-11-01',
+'changed' => '2018-09-04',
 );
 
 my $DEBUG = 1;
 
-#my $auroraurl = "http://www.aurora-service.eu/aurora-forecast/";
-#my $db = Irssi::get_irssi_dir() . '/scripts/auroras.db';
-my $db = "/home/laama/public_html/auroras.db";
+my $db = $ENV{HOME}.'/public_html/auroras.db';
 my @channels = ('#salamolo2', '#botti');
 
 sub getHelp {
-	return "!aurora|revontuli tulostaa kanavalle revontuliaktiviteetin ja ennustuksen. Aktiviteetti perustuu Kp-arvoon.	Mitä suurempi Kp, sen etelämmässä revontulia voi silloin nähdä.	!kuu, tulostaa kuun vaiheen, esim. 'täysikuu'.";
+	return '!aurora|revontuli tulostaa kanavalle revontuliaktiviteetin ja ennustuksen. Aktiviteetti perustuu Kp-arvoon.	Mitä suurempi Kp, sen etelämmässä revontulia voi silloin nähdä.	!kuu, tulostaa kuun vaiheen, esim. 'täysikuu'.';
 }
 
 sub pubmsg {
@@ -59,16 +54,16 @@ sub pubmsg {
 
 sub fetchAuroraData {
 	my $searchdate = time() - (60*60);			# max 1 hour ago
-	Irssi::print("search date: " . $searchdate) if $DEBUG;
+	Irssi::print('search date: ' . $searchdate) if $DEBUG;
 	my $fetchString = "select * from AURORAS where pvm > $searchdate ORDER BY PVM desc limit 1;";
 	my (@line) = KaaosRadioClass::readLinesFromDataBase($db, $fetchString);
-	Irssi::print ("Dump:") if $DEBUG;
+	Irssi::print ('Dump:') if $DEBUG;
 	print Dumper(@line) if $DEBUG;
 	my $kpnow = $line[0];
 	my $kpst = $line[1];
 	my $pvm = $line[2];
 	Irssi::print("auroras.pl: kpnow: $kpnow, kpst: $kpst, pvm: $pvm");
-	my $returnString = "Ei saatu tietoja!";
+	my $returnString = 'Ei saatu tietoja!';
 	if ($kpnow || $kpst) {
 		$returnString = "Kp arvo nyt: $kpnow, ennustus (1h): $kpst (Näkyvyys: Kp5=Helsinki, Kp4=Iisalmi, Kp3=Kemi)";
 	}
@@ -79,7 +74,7 @@ sub fetchAuroraData {
 sub conway {
 	# John Conway method
 	#my ($y,$m,$d);
-	chomp(my $y = `date +%Y`);         # TEST
+	chomp(my $y = `date +%Y`);
 	chomp(my $m = `date +%m`);
 	chomp(my $d = `date +%d`);
 
@@ -89,26 +84,23 @@ sub conway {
 	$r = (($r * 11) % 30) + $m + $d;
 	if ($m < 3) { $r += 2; }
 	$r -= 8.3;                                              # year > 2000
-	#test321 $r = ($r + 0.5) % 30;
+
 	$r = ($r + 0.5) % 29;	#test321
-	Irssi::print("auroras.pl Jäännös: $r") if $DEBUG;
-	#test123 
 	my $age = $r;
 	$r = 7/30 * $r + 1;
-	#my $age = $r;	# test123
-	Irssi::print("auroras.pl Jäännös2: $r") if $DEBUG;
+
 =pod
-      0: "New Moon",
-      1: "Waxing Crescent",
-      2: "First Quarter",
-      3: "Waxing Gibbous",
-      4: "Full Moon",
-      5: "Waning Gibbous",
-      6: "Last Quarter",
-      7: "Waning Crescent"
+      0: 'New Moon',
+      1: 'Waxing Crescent',
+      2: 'First Quarter',
+      3: 'Waxing Gibbous',
+      4: 'Full Moon',
+      5: 'Waning Gibbous',
+      6: 'Last Quarter',
+      7: 'Waning Crescent'
 =cut
 
-	my @moonarray = ("uusikuu", "kuun kasvava sirppi", "kuun ensimmäinen neljännes", "kasvava kuperakuu", "täysikuu", "laskeva kuperakuu", "kuun viimeinen neljännes", "kuun vähenevä sirppi");
+	my @moonarray = ('uusikuu', 'kuun kasvava sirppi', 'kuun ensimmäinen neljännes', 'kasvava kuperakuu', 'täysikuu', 'laskeva kuperakuu', 'kuun viimeinen neljännes', 'kuun vähenevä sirppi');
 	return $moonarray[$r] .", ikä: $age vrk.";
 }
 
