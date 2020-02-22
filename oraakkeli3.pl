f06c92259944d5075d1122e710ebc62e3979f8a9
centrifug@gmail.com <mini@mini.kissala>
2020-02-22 13:35:43 +0200

    clean

--- oraakkeli3.pl
+++ oraakkeli3.pl
@@ -4,7 +4,7 @@ use Irssi;
 use KaaosRadioClass;				# LAama1 9.10.2017
 use vars qw($VERSION %IRSSI);
 
-$VERSION = '0.321';
+$VERSION = '0.322';
 %IRSSI = (
         authors     => 'LAama1',
         contact     => 'LAama1@Ircnet',
@@ -27,7 +27,7 @@ sub pubmsg {
 		my $querystr = substr $msg, ((length $serverrec->{nick}) +2);
 		my $stats = KaaosRadioClass::fetchUrl("http://www.lintukoto.net/viihde/oraakkeli/index.php?html=0&kysymys=${querystr}",0);
 		#sleep(2); # take a nap before answering
-		$serverrec->command("MSG $target $nick: ${stats}") unless $stats = -1;
+		$serverrec->command("MSG $target $nick: ${stats}") unless $stats == -1;
 		Irssi::print("!oraakkeli request from $nick on channel $target: $querystr, answer: $stats");
 	}
 	return;
