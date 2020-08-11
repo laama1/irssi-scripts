64f8587a66bd6264ed06bfb994510441721b89a1
centrifug@gmail.com <14946062+laama1@users.noreply.github.com>
2020-08-11 22:22:41 +0300

    addquote v1.0b release

--- addquote.pl
+++ addquote.pl
@@ -22,7 +22,7 @@ my $db = Irssi::get_irssi_dir(). '/scripts/quotes.db';
 my $DEBUG = 1;
 
 use vars qw($VERSION %IRSSI);
-$VERSION = '20200404';
+$VERSION = '20200811';
 %IRSSI = (
 	authors     => 'LAama1',
 	contact     => 'ircnet: LAama1',
@@ -67,8 +67,7 @@ sub sayit {
 
 sub parseQuote {
 	my ($msg, $nick, $target, $server, @rest) = @_;
-	if($msg =~ /^!aq\s(.{1,470})/gi)
-	{
+	if($msg =~ /^!aq\s(.{1,470})/gi) {
 		#dp("parseQuote nick: $nick");
 		my $uusiquote = decode('UTF-8', $1);
 		my $pituus = length $uusiquote;
@@ -76,32 +75,75 @@ sub parseQuote {
 			return if KaaosRadioClass::floodCheck();
 			KaaosRadioClass::addLineToFile($tiedosto, $uusiquote);
 			saveToDB($nick, $uusiquote, $target);
-			Irssi::print($IRSSI{name}.": $msg request from $nick") if $DEBUG;
+			print($IRSSI{name}."> $msg request from $nick") if $DEBUG;
 			$server->command("msg $nick quote lisätty! $publicurl");
 			#sayit(':)');
 			$server->command("msg $target :)");
 		} else {
-			Irssi::print($IRSSI{name}.": $msg request from $nick (too long!)");
+			print($IRSSI{name}."> $msg request from $nick (too long!)");
 			$server->command("msg $nick quote liiian pitkä ($pituus)! max. about 470 merkkiä!");
+		}
+	} elsif ($msg =~ /^!rq (.{3,15})/gi) {
+		my $searchword = $1;
+		dp(__LINE__." searchword: $searchword");
+		my $data = KaaosRadioClass::readTextFile($tiedosto);
+		my @answers;
+		LINE: for (@$data) {
+			#next LINE unless ();
+			if ($_ =~ /$searchword/gi ) {
+				chomp (my $rimpsu = $_);
+				#dp(__LINE__." rimpsu: $rimpsu");
+				push @answers, $rimpsu;
+			}
+		}
+		my $amount_a = scalar @answers;
+		if ($amount_a > 0) {
+			dp("LÖYTYI!");
+			my $sayline = rand_line(@answers);
+			$server->command("MSG $target $sayline");			#splitlong.pl handles splitting message to many (if installed)
+		} else {
+			dp("EI LÖYTYNYT");
+			da(@answers);
+
 		}
 	} elsif ($msg =~ /^!rq/gi) {
 		my $data = KaaosRadioClass::readTextFile($tiedosto);
-		my $amount = @$data;
+		my $amount = scalar $data;
 		my $rand = int(rand($amount));
 		my $linecount = -1;
-		dp("addquote.pl amount: $amount, rand: $rand");
+		dp("amount: $amount, rand: $rand");
 		LINE: for (@$data) {
 			$linecount++;
 			next LINE unless ($rand == $linecount);
 			if($rand == $linecount) {
 				chomp (my $rimpsu = $_);
 				$server->command("MSG $target $rimpsu");			#splitlong.pl handles splitting message to many (if installed)
-				Irssi::print("vastasi: '$rimpsu' for $nick on channel: $target");
+				print($IRSSI{name}."> vastasi: '$rimpsu' for $nick on channel: $target");
 				last;
 			}
 		}
 	}
 	return;
+}
+
+# return random line from array
+sub rand_line {
+	my (@values, @rest) = @_;
+	my $amount = scalar @values;
+	my $rand = int(rand($amount));
+	my $linecount = -1;
+  LINEFOR: for (@values) {
+			$linecount++;
+			next LINEFOR unless ($rand == $linecount);
+			if($rand == $linecount) {
+				chomp (my $rimpsu = $_);
+				print($IRSSI{name}."> löytyi: $rimpsu");
+				return $rimpsu;
+				last;
+			}
+		}
+
+	return undef;
 }
 
 sub event_pubmsg {
@@ -115,14 +157,13 @@ sub createDB {
 	my $stmt = qq(CREATE VIRTUAL TABLE QUOTES using fts4(NICK, PVM, QUOTE,CHANNEL));
 	my $rv = $dbh->do($stmt);
 	if($rv < 0) {
-   		Irssi::print DBI::errstr;
+   		print $IRSSI{name}.'> '.DBI::errstr;
 	} else {
-   		Irssi::print $IRSSI{name}.": Table created successfully";
+   		print $IRSSI{name}.'> Table created successfully';
 	}
 	$dbh->disconnect();
 	return;
 }
-
 
 # Save to sqlite DB
 sub saveToDB {
