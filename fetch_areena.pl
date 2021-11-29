b3e420ae0a96ac753e24c8416ad195fab6752f98
centrifug@gmail.com <14946062+laama1@users.noreply.github.com>
2021-11-29 19:35:37 +0200

    fetch_areena upgrade

--- fetch_areena.pl
+++ fetch_areena.pl
@@ -5,32 +5,41 @@ use warnings;
 use Irssi;
 
 use vars qw($VERSION %IRSSI);
-$VERSION = '20200404';
+$VERSION = '20211106';
 %IRSSI = (
 	authors     => 'LAama1',
 	contact     => 'ircnet: LAama1',
 	name        => 'fetch_areena.pl',
-	description => 'Download video from YLE Areena.',
+	description => 'Download video and audio from YLE Areena or yle.fi.',
 	license     => 'Public Domain',
 	url         => '',
 	changed     => $VERSION,
 );
 
 my $myname = 'fetch_areena.pl';
-my $ylescript = 'yle-dl -qq --vfat --no-overwrite --destdir /mnt/music/areena --maxbitrate best --proxy=10.7.0.4:3128';
+#if you have Finnish proxy, use it here.
+my $proxy = "--proxy 10.7.0.1:3128";
+#my $proxy = "";
+my $download_dir = "/mnt/music/areena";
+my $ylescript = 'yle-dl -qq --vfat --no-overwrite --destdir '.$download_dir.' --maxbitrate best';
+
+print($IRSSI{name}.'> download dir: '.$download_dir);
 
 sub sig_msg_pub {
 	my ($server, $msg, $target) = @_;
 	if ($msg =~ /(https?:\/\/areena\.yle\.fi\/\S+)/i) {
 		cmd_start_dl($1);
-	} elsif ($msg =~ /(https:\/\/yle\.fi\S+)/i) {
+	} elsif ($msg =~ /(https?:\/\/yle\.fi\S+)/i) {
+		cmd_start_dl($1);
+	} elsif ($msg =~ /(https?:.*\.yle\.fi\S+)/i) {
+		#print($IRSSI{name}.'> debug: hips');
 		cmd_start_dl($1);
 	}
 }
 
 sub cmd_start_dl {
 	my ($url, @rest) = @_;
-	Irssi::print($IRSSI{name}.' fetching: '. $url);
+	print($IRSSI{name}.'> fetching: '. $url);
 	Irssi::command("exec -name yle -nosh $ylescript $url");
 	#Irssi::command("exec -close yle");	# detach
 	# -interactive
