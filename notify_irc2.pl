b510a0e0f0182975b81590c9e2b40bdde6f12891
centrifug@gmail.com <14946062+laama1@users.noreply.github.com>
2023-03-11 17:50:45 +0200

    new script, can take commands from socket

---
+++ notify_irc2.pl
@@ -0,0 +1,88 @@
+use warnings;
+use strict;
+use Irssi;
+use utf8;
+#use KaaosRadioClass;
+#use IO::Socket;
+use Socket;
+#use Fcntl;
+
+use vars qw($VERSION %IRSSI);
+$VERSION = "0.2";
+%IRSSI = (
+	authors	=> 'laama',
+	contact	=> 'kaaosradio ircnet',
+	name	=> 'notify_irc',
+	description	=> 'Skripti lukee sock-tiedostoa ja ilmoittaa irciin jos jotain kivaa tapahtuu',
+	license	=> 'BSD',
+	changed	=> '2023-03-08',
+	url		=> 'https://www.kaaosradio.fi'
+);
+
+my $DEBUG = 1;
+my $lastmesg = '';
+
+my $kaaos_server;
+my $handle = undef;
+
+
+
+start_server();
+sub start_server {
+	Irssi::print("KAaosd - starting...");
+  	$kaaos_server = IO::Socket::INET->new( Proto => 'tcp', LocalAddr => '127.0.0.1' , LocalPort => 12347, Listen => SOMAXCONN, ReusePort => 1) 
+		or print "Can't bind to port 12347, $@";
+  	if(!$kaaos_server) {
+    	Irssi::print("KAaosd - couldn't start server, $@", MSGLEVEL_CLIENTERROR);
+    	return;
+  	}
+
+  Irssi::print(sprintf("KAaosd - waiting for connections on %s:%s...", $kaaos_server->sockhost, $kaaos_server->sockport));
+  $handle = Irssi::input_add($kaaos_server->fileno, INPUT_READ, 'handle_connection', $kaaos_server);
+}
+
+sub handle_connection {
+	my $sock = $_[0]->accept;
+  	my $iaddr = inet_aton($sock->peerhost); # or whatever address
+  	my $peer  = gethostbyaddr($iaddr, AF_INET); # $sock->peerhost;
+	Irssi::print("KAaosd - handling connection from $peer");
+
+	$sock->autoflush(1);
+	my $incoming = <$sock>;
+	Irssi::print("KAaosd - got: $incoming from: $peer");
+	parse_msg($incoming);
+}
+
+# check the socket for data and act upon it
+sub parse_msg {
+	my ($msg, @rest) = @_;
+	DP("Got message from socket: $msg") if $msg;
+	if ($msg =~ /nytsoivideo (.*)$/ && $msg ne $lastmesg) {
+		DP("Joku päivitti videostreamin metan: $1");
+		Irssi::signal_emit('krnytsoivideo-remote-msg', $1);
+	} elsif ($msg =~ /nytsoivideotwitch (.*)$/ && $msg ne $lastmesg) {
+		DP("Joku päivitti twitch-tiedon: $1");
+		Irssi::signal_emit('krnytsoivideotwitch-remote-msg', $1);
+	} elsif ($msg =~ /nytsoi (.*)$/ && $msg ne $lastmesg) {
+		DP("Joku päivitti icecastin: $1");
+		Irssi::signal_emit('krnytsoi-remote-msg', $1);
+	}
+	$lastmesg = $msg;
+}
+
+sub DP {
+	return unless $DEBUG == 1;
+	print($IRSSI{name}." debug> @_");
+	return;
+}
+
+my $signal_config_hash1 = { 'krnytsoi-remote-msg' => [ qw/iobject string/ ] };
+Irssi::signal_register($signal_config_hash1);
+
+my $signal_config_hash2 = { 'krnytsoivideo-remote-msg' => [ qw/iobject string/ ] };
+Irssi::signal_register($signal_config_hash2);
+
+my $signal_config_hash3 = { 'krnytsoivideotwitch-remote-msg' => [ qw/iobject string/ ] };
+Irssi::signal_register($signal_config_hash3);
+
+print($IRSSI{name}."> v. $VERSION Loaded!");
