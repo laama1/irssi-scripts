73497ad2e8d6add47e92f76215fd7bbbb80e5da6
centrifug@gmail.com <14946062+laama1@users.noreply.github.com>
2023-08-15 21:47:21 +0300

    upd

--- dronebl_check.pl
+++ dronebl_check.pl
@@ -24,7 +24,7 @@ my $scriptfile = Irssi::get_irssi_dir().'/scripts/checkdnsbl.sh';
 # when you join a channel
 sub event_chan_joined {
     my ($channel, @rest) = @_;
-    print "Jes.";
+    print "Jes. i joined: ";
     print Dumper $channel;
     print "--------------";
     print Dumper @rest ;
@@ -42,20 +42,22 @@ sub dronebl_check {
 # when user joined your channel
 sub event_msg_joined {
     my ($server, $channel, $nick, $address, $account, $realname) = @_;
-    my @ip_parts = split '\@', $address;
+    #my @ip_parts = split '\@', $address;
+    my @ip_parts = split '@', $address;
     $server->send_raw("whois $nick");
 
     my $host = $ip_parts[1];
     my $ident = $ip_parts[0];
     print "Got nick: $nick, ident: $ident, host: $host";
-    dronebl_check($host);
     save_stuff($nick, $ident, $host);
+    dronebl_check($host);
+    #save_stuff($nick, $ident, $host);
 }
 
 sub event_whois {
     my ($server, $data, $srv_addr, @rest) = @_;
     my ($me, $nick, $user, $host) = split(" ", $data);
-    my $network = $server->{tag};
+    #my $network = $server->{tag};
     
     #$nick = fc $nick;
     print("Nick: $nick, me: $me, user: $user, host: $host, server address: $srv_addr");
@@ -67,7 +69,7 @@ sub do_the_kick {
 }
 
 sub save_stuff {
-    my ($nick, $ident, $ip, @channels);
+    my ($nick, $ident, $ip, @channels) = @_;
     print("Saving: $nick, $ident, $ip");
     KaaosRadioClass::addLineToFile($statsfile, time.';'.$nick. ';'. $ident.';'.$ip.';');
 }
